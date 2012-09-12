e8fb0bf0d1d16ba909da88ec544df0db16faa775
David Schneider <david.schneider@picle.org>
2012-09-12 10:35:52 +0200

    write some more

---
+++ talk/dls2012/demo/analytics.py
@@ -0,0 +1,16 @@
+
+from reloader import ReloadHack
+from io import view
+from background import Background
+from foreground import foreground
+
+class Tracker(ReloadHack):
+    def __init__(self):
+        self.background = Background()
+
+    def update(self, frame):
+        self.background.update(frame)
+        fg = foreground(frame, self.background.image)
+        #view(self.background.image)
+        view(255 * fg)
+
